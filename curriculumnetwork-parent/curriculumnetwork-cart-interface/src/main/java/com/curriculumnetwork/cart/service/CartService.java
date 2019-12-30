c4911c7160f52a81e03ddb6dff99142f2fd42ce9
octopus <345293721@qq.com>
2019-12-30 11:40:51 +0800

    Update CartService.java

--- curriculumnetwork-parent/curriculumnetwork-cart-interface/src/main/java/com/curriculumnetwork/cart/service/CartService.java
+++ curriculumnetwork-parent/curriculumnetwork-cart-interface/src/main/java/com/curriculumnetwork/cart/service/CartService.java
@@ -6,39 +6,39 @@ import com.curriculumnetwork.pojo.group.Cart;
 
 /**
  * 购物车服务接口
- * @author Administrator
+ * @author zhangyu
  *
  */
 public interface CartService {
 
 	/**
 	 * 添加课程网到购物车
-	 * @param list
-	 * @param itemId
-	 * @param num
-	 * @return
+	 * @param cartList 购物车链表
+	 * @param itemId   商品id
+	 * @param num	   数量
+	 * @return	   课程链表
 	 */
 	public List<Cart> addGoodsToCartList(List<Cart> cartList,Long itemId,Integer num);
 	
 	/**
 	 * 从redis中提取购物车列表
-	 * @param username
-	 * @return
+	 * @param username 用户名
+	 * @return  用户的购物车
 	 */
 	public List<Cart> findCartListFromRedis(String username);
 	
 	/**
 	 * 将购物车列表存入redis
-	 * @param username
-	 * @param cartList
+	 * @param username  用户名
+	 * @param cartList  购物车信息	
 	 */
 	public void saveCartListToRedis(String username,List<Cart> cartList);
 	
 	/**
 	 * 合并购物车
-	 * @param cartList1
-	 * @param cartList2
-	 * @return
+	 * @param cartList1 购物车1
+	 * @param cartList2 购物车2
+	 * @return  合并之后购物车
 	 */
 	public List<Cart> mergeCartList(List<Cart> cartList1,List<Cart> cartList2);
 	 
